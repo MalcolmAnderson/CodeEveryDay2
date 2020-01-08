3575236eefff1632cb16bd5d8153d5b1e4d2f2bf
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-08 08:13:15 -0800

    Having issues setting up my environment.

---
+++ 100AlgorithmsChallenge/AAbsoluteValuesSumMinimization/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public static bool IsOdd(int value)
+    {
+        return value % 2 == 1;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # This is my c# 100 algorithm challenge workbench.
 
+## Open Questions
+* How to setup vscode to best support a TDD style algorithms dojo
+* How to setup a test runner to automatically run (quickly) on save.
 
 ## Areas   
 * Code Conversion Toolkit  
