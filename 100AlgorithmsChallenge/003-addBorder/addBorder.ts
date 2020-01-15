69fef4815b48721f0593833526d9c0982bfc9653
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-15 08:43:49 -0800

    I'm sure there are better ways

--- 100AlgorithmsChallenge/003-addBorder/addBorder.ts
+++ 100AlgorithmsChallenge/003-addBorder/addBorder.ts
@@ -1,5 +1,18 @@
+function test(): number {
+    return -1;
+}
 function addBorder(picture: string[]): string[] {
-
+    let topBorder: string = "";
+    for (let i: number = 0; i < picture[0].length + 2; i++) {
+        topBorder += "*";
+    }
+    const output: string[] = [];
+    output.push(topBorder);
+    for (let i: number = 0; i < picture.length; i++) {
+        output.push("*" + picture[i] + "*");
+    }
+    output.push(topBorder);
+    return output;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
