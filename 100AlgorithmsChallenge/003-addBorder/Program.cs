b369c1950c970fab9f72fbba8d5334863b7147c4
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-16 17:00:55 -0800

    added dotnet start template

---
+++ 100AlgorithmsChallenge/003-addBorder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = new string[] { "abc", "ded" };
+        string[] output = addBorder(input);
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.WriteLine(output[i]);
+        }
+    }
+
+    static string[] addBorder(string[] picture)
+    {
+        List<string> outputList = new List<string>();
+
+
+
+        return new string[] { "keep", "trying" };
+    }
+
+}
