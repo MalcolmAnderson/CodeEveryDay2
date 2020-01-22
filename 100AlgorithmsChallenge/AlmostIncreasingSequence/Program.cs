1f9b0d1ea41b64a3a5c47c6832142ab5dd75ee2b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-22 06:25:06 -0800

    Prep for AlmostIncreasingSequence

---
+++ 100AlgorithmsChallenge/AlmostIncreasingSequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = new int[] { 1, 3, 2, 1 };
+        Console.WriteLine(AlmostIncreasingSequence(input));
+        input = new int[] { 1, 3, 2 };
+        Console.WriteLine(AlmostIncreasingSequence(input));
+    }
+
+    static bool AlmostIncreasingSequence(int[] sequence)
+    {
+        return null;
+    }
+
+}
