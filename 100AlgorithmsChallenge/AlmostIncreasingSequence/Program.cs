928c60f1115f3981ec2dbf9706ddc615b5a183be
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-24 07:58:08 -0800

    completed AlmostIncreasingSequence

--- 100AlgorithmsChallenge/AlmostIncreasingSequence/Program.cs
+++ 100AlgorithmsChallenge/AlmostIncreasingSequence/Program.cs
@@ -4,15 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] input = new int[] { 1, 3, 2, 1 };
+        int[] input = new int[] { 1, 3, 2, 1 }; // false
         Console.WriteLine(AlmostIncreasingSequence(input));
-        input = new int[] { 1, 3, 2 };
+        input = new int[] { 1, 3, 2 }; // true
         Console.WriteLine(AlmostIncreasingSequence(input));
     }
 
     static bool AlmostIncreasingSequence(int[] sequence)
     {
-        return null;
+        // track last inceasing digit
+        // track number of violation
+        int last = 0;
+        int violations = 0;
+        // iterate over the array
+        for (int i = 0; i < sequence.Length; i++)
+        {
+
+            //      if current > last
+            if (sequence[i] > last)
+            {
+                //          last = current
+                last = sequence[i];
+                //      else
+            }
+            else
+            {
+                //          violations ++
+                violations++;
+            }
+        }
+        // if violations > 1
+        if (violations > 1)
+        {
+            //      return false
+            return false;
+        }
+        //  return true
+        return true;
     }
 
 }
